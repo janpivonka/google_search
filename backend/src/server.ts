17a6f54dc7b78601d7c0e786902137cfaae0f028
Jan Pivoňka <jan.pivonka.job@gmail.com>
2025-12-27 14:45:18 +0100

    corrects server for railway

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
 }
