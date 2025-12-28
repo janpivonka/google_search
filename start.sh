e8935c5b890953e98391d90914435271814a0fba
Jan Pivoňka <jan.pivonka.job@gmail.com>
2025-12-28 18:15:38 +0100

    corrects frontend port for docker.dev start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ docker compose -f docker-compose.yml up --build -d
 echo "Čekám na spuštění serverů..."
 sleep 5
 
-powershell.exe start http://localhost:80/
+powershell.exe start http://localhost:5173/
 powershell.exe start http://localhost:3001/
 
 echo "Produkční servery spuštěny!"
